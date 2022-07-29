ec42725bac3185644816a2c3f3241e3e73a61d1e
dilller <dmytropomyluiko@gmail.com>
2022-07-29 20:47:35 +0300

    Create JWTUtil

---
+++ src/main/java/com/warehousekeeper/root/security/JWTUtil.java
@@ -0,0 +1,40 @@
+package com.warehousekeeper.root.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+    @Value("${secret}")
+    private String secret;
+
+    public String generateToken(String username){
+        // 8 hours
+        Date expirationDate = Date.from(ZonedDateTime.now().plusMinutes(480).toInstant());
+        return JWT.create()
+                .withSubject("User details")
+                .withClaim("username", username)
+                .withIssuedAt(new Date())
+                .withIssuer("WarehouseKeeper")
+                .withExpiresAt(expirationDate)
+                .sign(Algorithm.HMAC256(secret));
+    }
+
+    public String validateTokenAndRetrieveClaim(String token) throws JWTVerificationException {
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret))
+                .withSubject("User details")
+                .withIssuer("WarehouseKeeper")
+                .build();
+
+        DecodedJWT decodedJWT = verifier.verify(token);
+        return decodedJWT.getClaim("username").asString();
+    }
+}
